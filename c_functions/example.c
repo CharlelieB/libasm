40f80a7795c721b424c4f350802c94bffc95b639
Charlelie BESSONNET <cbessonn@paul-f5Br4s1.clusters.42paris.fr>
2024-10-28 12:04:59 +0100

    add examples

--- c_functions/example.c
+++ c_functions/example.c
@@ -1,4 +1,15 @@
+/*
 
+ft_strlen (man 3 strlen)
+ft_strcpy (man 3 strcpy)
+ft_strcmp (man 3 strcmp)
+ft_write (man 2 write)
+ft_read (man 2 read)
+ft_strdup (man 3 strdup)
+
+*/
+
+//FT_STRLEN
 
 size_t	ft_strlen(const char *s)
 {
@@ -10,7 +21,42 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+//FT_STRCPY
 
+char *strcpy(char *dest, const char *src)
+{
+	size_t i;
+	
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return dest;
+ }
+
+//FT_STRCMP
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	while ((*s1 || *s2))
+	{
+		if ((unsigned char)*s1 != (unsigned char)*s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+//FT_WRITE
+
+ssize_t write(int fd, const void *buf, size_t count);
+
+//FT_READ
+
+ssize_t read(int fd, void *buf, size_t count);
+
+
+//FT_STRDUP
 
 char	*ft_strdup(const char *s)
 {
